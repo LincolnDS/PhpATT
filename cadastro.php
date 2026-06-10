99f30a71c29ca313634350e27f24cd0ec05b1a8f
LincolnDS <GabrielLincoln.23@outlook.com>
2026-06-09 21:58:52 -0300

    Sou zika

--- cadastro.php
+++ cadastro.php
@@ -1,23 +1,20 @@
 <?php 
-
 	$host = "localhost";
 	$user = "arroz";
 	$pwd = "123456";
 	$db = "dbloja";
 
 	$con = mysqli_connect($host, $user, $pwd, $db);
-	
 	if (mysqli_connect_errno()){
 		echo "Não foi possivel acessar o banco de dados" .mysqli_connect_error();	
 
 	}else{
 		echo "Banco de dados conectado com sucesso!";
-		$sql = "insert into tbPessoa(nome, email, idade)
-		values('Etecia', 'sac@etecia.com')";
-		mysql_query($con, $sql);
+		$sql = "INSERT INTO tbpessoa(nome, email, idade) VALUES('$_POST[nome]','$_POST[email]', '$_POST[idade]')";
+
+		mysqli_query($con, $sql);
+
+		mysqli_close($con); 
 	}
-
-
-
 
  ?>
---
+++ inserindo_dados.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Inserindo dados PHP</title>
+</head>
+<body>
+	<form action="cadastro.php" method="post">
+		Nome:
+		<input type="text" name="nome">
+		<br><br>
+		E-mail:
+		<input type="E-mail" name="email">
+		<br><br>
+		Idade:
+		<input type="Idade" name="idade">
+		<br><br>
+		<input type="submit" name="Cadastrar">		
+
+	</form>
+</body>
+</html>
